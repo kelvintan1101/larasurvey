8e38f7b5cc28df1641e70ca2e1ec4dfb016496c1
Kelvin <kelvintan1101@gmail.com>
2024-07-29 17:59:24 +0800

    chore: Update Surveys and Dashboard views with header and main sections

--- vue/src/router/index.js
+++ vue/src/router/index.js
@@ -2,12 +2,19 @@ import { createRouter, createWebHistory } from "vue-router";
 import Dashboard from "../views/Dashboard.vue";
 import Login from "../views/Login.vue";
 import Register from "../views/Register.vue";
+import DefaultLayout from "../components/DefaultLayout.vue";
+import Surveys from "../views/Surveys.vue";
 
 const routes = [
   {
     path: "/",
+    redirect: "/dashboard",
     name: "Dashboard",
-    component: Dashboard,
+    component: DefaultLayout,
+    children: [
+      { path: "/dashboard", name: "Dashboard", component: Dashboard },
+      { path: "/surveys", name: "Surveys", component: Surveys },
+    ],
   },
   {
     path: "/login",
