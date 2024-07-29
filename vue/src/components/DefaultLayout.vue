7f4829e81999d032da14bd93a005617be788e289
Kelvin <kelvintan1101@gmail.com>
2024-07-30 00:24:50 +0800

    chore: Add logout functionality to AuthController and update user token handling

--- vue/src/components/DefaultLayout.vue
+++ vue/src/components/DefaultLayout.vue
@@ -172,8 +172,11 @@ export default {
     const router = useRouter();
 
     function logout() {
-      store.commit("logout");
-      router.push({ name: "Login" });
+      store.dispatch("logout").then(() => {
+        router.push({
+          name: "Login",
+        });
+      });
     }
 
     return {
