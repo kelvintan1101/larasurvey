f7606f0eefde06bc8e8f4985dc1b84d2d7e9b67b
Kelvin <kelvintan1101@gmail.com>
2024-08-27 15:11:44 +0800

    refactor: Update Alert component to use SVG icons instead of images

--- vue/src/views/Login.vue
+++ vue/src/views/Login.vue
@@ -14,10 +14,7 @@
 
   <div class="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
     <form class="space-y-6" @submit="login">
-      <div
-        v-if="errorMsg"
-        class="flex items-center justify-between py-3 px-5 bg-red-500 text-white rounded"
-      >
+      <Alert v-if="errorMsg">
         {{ errorMsg }}
         <span
           @click="errorMsg = ''"
@@ -38,7 +35,8 @@
             />
           </svg>
         </span>
-      </div>
+      </Alert>
+
       <div>
         <label
           for="email"
@@ -95,9 +93,35 @@
 
       <div>
         <button
+          :disabled="loading"
           type="submit"
           class="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+          :class="{
+            'cursor-not-allowed': loading,
+            'hover:bg-indigo-500': loading,
+          }"
         >
+          <svg
+            v-if="loading"
+            class="animate-spin -ml-1 mr-3 h-5 w-5 text-white"
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+          >
+            <circle
+              class="opacity-25"
+              cx="12"
+              cy="12"
+              r="10"
+              stroke="currentColor"
+              stroke-width="4"
+            ></circle>
+            <path
+              class="opacity-75"
+              fill="currentColor"
+              d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+            ></path>
+          </svg>
           Sign in
         </button>
       </div>
@@ -119,6 +143,7 @@
 import store from "../store";
 import { useRouter } from "vue-router";
 import { ref } from "vue";
+import Alert from "../components/Alert.vue";
 
 const router = useRouter();
 const user = {
@@ -127,18 +152,23 @@ const user = {
   remember: false,
 };
 
+const loading = ref(false);
+
 let errorMsg = ref("");
 
 function login(e) {
   e.preventDefault();
+  loading.value = true;
   store
     .dispatch("login", user)
     .then(() => {
+      loading.value = false;
       router.push({
         name: "Dashboard",
       });
     })
     .catch((err) => {
+      loading.value = false;
       errorMsg.value = err.response.data.error;
     });
 }
--- vue/src/views/Register.vue
+++ vue/src/views/Register.vue
@@ -14,6 +14,16 @@
 
   <div class="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
     <form class="space-y-6" @submit="register">
+      <Alert
+        v-if="Object.keys(errors).length"
+        class="flex-col items-stretch text-sm"
+      >
+        <div v-for="(field, i) of Object.keys(errors)" :key="i">
+          <div v-for="(error, ind) of errors[field] || []" key="ind">
+            * {{ error }}
+          </div>
+        </div>
+      </Alert>
       <div>
         <label
           for="fullname"
@@ -94,9 +104,35 @@
 
       <div>
         <button
+          :disabled="loading"
           type="submit"
           class="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+          :class="{
+            'cursor-not-allowed': loading,
+            'hover:bg-indigo-500': loading,
+          }"
         >
+          <svg
+            v-if="loading"
+            class="animate-spin -ml-1 mr-3 h-5 w-5 text-white"
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+          >
+            <circle
+              class="opacity-25"
+              cx="12"
+              cy="12"
+              r="10"
+              stroke="currentColor"
+              stroke-width="4"
+            ></circle>
+            <path
+              class="opacity-75"
+              fill="currentColor"
+              d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+            ></path>
+          </svg>
           Sign up
         </button>
       </div>
@@ -117,6 +153,8 @@
 <script setup>
 import store from "../store";
 import { useRouter } from "vue-router";
+import { ref } from "vue";
+import Alert from "../components/Alert.vue";
 
 const router = useRouter();
 const user = {
@@ -126,12 +164,25 @@ const user = {
   password_confirmation: "",
 };
 
+const loading = ref(false);
+const errors = ref({});
+
 function register(e) {
   e.preventDefault();
-  store.dispatch("register", user).then((res) => {
-    router.push({
-      name: "Dashboard",
+  loading.value = true;
+  store
+    .dispatch("register", user)
+    .then((res) => {
+      loading.value = false;
+      router.push({
+        name: "Dashboard",
+      });
+    })
+    .catch((err) => {
+      loading.value = false;
+      if (err.response.status === 422) {
+        errors.value = err.response.data.errors;
+      }
     });
-  });
 }
 </script>
